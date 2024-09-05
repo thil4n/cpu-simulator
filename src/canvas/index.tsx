2bdb5c53e20cfd9dcb2653e4277d695528cc52bc
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2024-09-05 15:54:49 +0530

    fix bug

--- src/canvas/index.tsx
+++ src/canvas/index.tsx
@@ -64,6 +64,27 @@ const Canvas = () => {
         rflags: { key: "rflags", data: null, desc: "R Flags Register" },
     };
 
+    const gp_registers = [
+        "rax",
+        "rbx",
+        "rcx",
+        "rdx",
+        "rsi",
+        "rdi",
+        "rbp",
+        "rsp",
+    ];
+    const adgp_registers = [
+        "r8",
+        "r9",
+        "r10",
+        "r11",
+        "r12",
+        "r13",
+        "r14",
+        "r15",
+    ];
+
     const showMemory = (startAddr: number, highlightLength = 0) => {
         const endAddr = startAddr + 200;
         let memory = [];
@@ -76,6 +97,110 @@ const Canvas = () => {
         }
 
         setMemory(memory);
+    };
+
+    const is64BitRegister = (str: string) => {
+        const gpRegisters = [
+            "rax",
+            "rbx",
+            "rcx",
+            "rdx",
+
+            "rsi",
+            "rdi",
+
+            "rbp",
+            "rsp",
+        ];
+
+        const adgpRegisters = [
+            "r8",
+            "r9",
+            "r10",
+            "r11",
+            "r12",
+            "r13",
+            "r14",
+            "r15",
+        ];
+
+        const sppRegisters = ["rip", "rflags"];
+
+        return (
+            gpRegisters.includes(str) ||
+            adgpRegisters.includes(str) ||
+            sppRegisters.includes(str)
+        );
+    };
+
+    const is32BitRegister = (str: string) => {
+        const gpRegisters = [
+            "eax",
+            "ebx",
+            "ecx",
+            "edx",
+
+            "esi",
+            "edi",
+
+            "ebp",
+            "esp",
+        ];
+
+        const adgpRegisters = [
+            "r8d",
+            "r9d",
+            "r10d",
+            "r11d",
+            "r12d",
+            "r13d",
+            "r14d",
+            "r15d",
+        ];
+
+        return gpRegisters.includes(str) || adgpRegisters.includes(str);
+    };
+
+    const is16BitRegister = (str: string) => {
+        const gpRegisters = ["ax", "bx", "cx", "dx", "si", "di", "bp", "sp"];
+
+        const adgpRegisters = [
+            "r8w",
+            "r9w",
+            "r10w",
+            "r11w",
+            "r12w",
+            "r13w",
+            "r14w",
+            "r15w",
+        ];
+
+        return gpRegisters.includes(str) || adgpRegisters.includes(str);
+    };
+
+    const is8BitRegister = (str: string) => {
+        const gpRegistersLower = ["al", "bl", "cl", "dl"];
+        const gpRegistersHigher = ["ah", "bh", "ch", "dh"];
+
+        const idgpRegisters = ["sil", "dil"];
+
+        const adgpRegisters = [
+            "r8b",
+            "r9b",
+            "r10b",
+            "r11b",
+            "r12b",
+            "r13b",
+            "r14b",
+            "r15b",
+        ];
+
+        return (
+            gpRegistersLower.includes(str) ||
+            gpRegistersHigher.includes(str) ||
+            idgpRegisters.includes(str) ||
+            adgpRegisters.includes(str)
+        );
     };
 
     const isRegister = (str: string): boolean => {
