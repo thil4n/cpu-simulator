d4f1e96ac4cae0a151851b4cf42accbcb4d72125
Thilan <thil4n@gmail.com>
2025-03-26 15:08:48 +0530

    add and sub instructions

--- src/canvas/index.tsx
+++ src/canvas/index.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, SetStateAction } from "react";
 import { ToastContainer } from "react-toastify";
-import { toast } from "react-toastify";
 
 import { Button, Input, Loader, Modal } from "@components";
 
@@ -160,6 +159,21 @@ const Canvas = () => {
     }
   };
 
+  const pop = (operand: any) => {
+    if (isRegister(operand)) {
+      //intcpy(registers.rsp.data, registers[operand].data);
+      consoleLog(
+        `Copying the value of top of the stack to the ${operand} register.`
+      );
+    } else if (isMemoryAddress(operand)) {
+      consoleLog(
+        `Copying the value of top of the stack to the address ${operand}.`
+      );
+    } else {
+      consoleError("Invalid operand given for the pop operation.");
+    }
+  };
+
   const mov = (op_1: any, op_2: any) => {
     // op_1 | op_2 : reg
     if (isRegister(op_1) && isRegister(op_2)) {
@@ -199,14 +213,171 @@ const Canvas = () => {
 
     // Invalid operation
     else {
-      toast.info(" Invalid operands");
+      consoleError("Invalid operands given for the mov operation.");
     }
   };
 
-  const pop = (dest: any) => {};
-  const add = (src: any, dest: any) => {};
-  const sub = (src: any, dest: any) => {};
-  const cmp = (src: any, dest: any) => {};
+  const add = (src: any, dest: any) => {
+    // src & dest : reg
+    if (isRegister(src) && isRegister(dest)) {
+      registers[dest].data += registers[src].data;
+      consoleLog(`ADD ${src} -> ${dest} (Register to Register)`);
+    }
+
+    // src : reg | dest : mem addr
+    else if (isRegister(src) && isMemoryAddress(dest)) {
+      let result = registers[src].data + intcpy(parseAddr(dest));
+      intcpy(parseAddr(dest), result);
+      consoleLog(`ADD ${src} -> ${parseAddr(dest)} (Register to Memory)`);
+    }
+
+    // src : mem | dest : reg
+    else if (isMemoryAddress(src) && isRegister(dest)) {
+      registers[dest].data += intcpy(parseAddr(src));
+      consoleLog(`ADD ${parseAddr(src)} -> ${dest} (Memory to Register)`);
+    }
+
+    // src & dest : mem addr
+    else if (isMemoryAddress(src) && isMemoryAddress(dest)) {
+      let result = intcpy(parseAddr(src)) + intcpy(parseAddr(dest));
+      intcpy(parseAddr(dest), result);
+      consoleLog(
+        `ADD ${parseAddr(src)} -> ${parseAddr(dest)} (Memory to Memory)`
+      );
+    }
+
+    // src : num value | dest : reg
+    else if (isNumericValue(src) && isRegister(dest)) {
+      registers[dest].data += parseInt(src);
+      consoleLog(`ADD ${src} -> ${dest} (Immediate to Register)`);
+    }
+
+    // src : num value | dest : mem addr
+    else if (isNumericValue(src) && isMemoryAddress(dest)) {
+      let result = parseInt(src) + intcpy(parseAddr(dest));
+      intcpy(parseAddr(dest), result);
+      consoleLog(`ADD ${src} -> ${parseAddr(dest)} (Immediate to Memory)`);
+    }
+
+    // Invalid operation
+    else {
+      consoleError("Invalid operands given for the ADD operation.");
+    }
+  };
+
+  const sub = (src: any, dest: any) => {
+    // src & dest : reg
+    if (isRegister(src) && isRegister(dest)) {
+      registers[dest].data -= registers[src].data;
+      consoleLog(`SUB ${src} -> ${dest} (Register to Register)`);
+    }
+
+    // src : reg | dest : mem addr
+    else if (isRegister(src) && isMemoryAddress(dest)) {
+      let result = intcpy(parseAddr(dest)) - registers[src].data;
+      intcpy(parseAddr(dest), result);
+      consoleLog(`SUB ${src} -> ${parseAddr(dest)} (Register from Memory)`);
+    }
+
+    // src : mem | dest : reg
+    else if (isMemoryAddress(src) && isRegister(dest)) {
+      registers[dest].data -= intcpy(parseAddr(src));
+      consoleLog(`SUB ${parseAddr(src)} -> ${dest} (Memory to Register)`);
+    }
+
+    // src & dest : mem addr
+    else if (isMemoryAddress(src) && isMemoryAddress(dest)) {
+      let result = intcpy(parseAddr(dest)) - intcpy(parseAddr(src));
+      intcpy(parseAddr(dest), result);
+      consoleLog(
+        `SUB ${parseAddr(src)} -> ${parseAddr(dest)} (Memory to Memory)`
+      );
+    }
+
+    // src : num value | dest : reg
+    else if (isNumericValue(src) && isRegister(dest)) {
+      registers[dest].data -= parseInt(src);
+      consoleLog(`SUB ${src} -> ${dest} (Immediate to Register)`);
+    }
+
+    // src : num value | dest : mem addr
+    else if (isNumericValue(src) && isMemoryAddress(dest)) {
+      let result = intcpy(parseAddr(dest)) - parseInt(src);
+      intcpy(parseAddr(dest), result);
+      consoleLog(`SUB ${src} -> ${parseAddr(dest)} (Immediate to Memory)`);
+    }
+
+    // Invalid operation
+    else {
+      consoleError("Invalid operands given for the SUB operation.");
+    }
+  };
+
+  const cmp = (src: any, dest: any) => {
+    let result;
+
+    // src & dest : reg
+    if (isRegister(src) && isRegister(dest)) {
+      result = registers[dest].data - registers[src].data;
+      consoleLog(`CMP ${src} -> ${dest} (Register to Register)`);
+    }
+
+    // src : reg | dest : mem addr
+    else if (isRegister(src) && isMemoryAddress(dest)) {
+      result = intcpy(parseAddr(dest)) - registers[src].data;
+      consoleLog(`CMP ${src} -> ${parseAddr(dest)} (Register to Memory)`);
+    }
+
+    // src : mem | dest : reg
+    else if (isMemoryAddress(src) && isRegister(dest)) {
+      result = registers[dest].data - intcpy(parseAddr(src));
+      consoleLog(`CMP ${parseAddr(src)} -> ${dest} (Memory to Register)`);
+    }
+
+    // src & dest : mem addr
+    else if (isMemoryAddress(src) && isMemoryAddress(dest)) {
+      result = intcpy(parseAddr(dest)) - intcpy(parseAddr(src));
+      consoleLog(
+        `CMP ${parseAddr(src)} -> ${parseAddr(dest)} (Memory to Memory)`
+      );
+    }
+
+    // src : num value | dest : reg
+    else if (isNumericValue(src) && isRegister(dest)) {
+      result = registers[dest].data - parseInt(src);
+      consoleLog(`CMP ${src} -> ${dest} (Immediate to Register)`);
+    }
+
+    // src : num value | dest : mem addr
+    else if (isNumericValue(src) && isMemoryAddress(dest)) {
+      result = intcpy(parseAddr(dest)) - parseInt(src);
+      consoleLog(`CMP ${src} -> ${parseAddr(dest)} (Immediate to Memory)`);
+    }
+
+    // Invalid operation
+    else {
+      consoleError("Invalid operands given for the CMP operation.");
+      return;
+    }
+
+    // Set flags based on the result of the comparison
+    setFlags(result);
+  };
+
+  const setFlags = (result: number) => {
+    // Zero flag: Set if the result is zero
+    const zeroFlag = result === 0;
+
+    // Negative flag: Set if the result is negative
+    const negativeFlag = result < 0;
+
+    // Carry flag: Set if no borrow occurred (unsigned comparison)
+    const carryFlag = result >= 0;
+
+    consoleLog(
+      `Flags - Zero: ${zeroFlag}, Negative: ${negativeFlag}, Carry: ${carryFlag}`
+    );
+  };
 
   const parseAssembly = () => {
     const instruction = formData.assemblyInput;
@@ -214,8 +385,8 @@ const Canvas = () => {
     const { operation, operandOne, operandTwo } = parseSingleLine(instruction);
 
     consoleLog(
-      `Operation : ${operation} |  Operand One : ${operandOne} ${
-        operandTwo ? " | Operand Two : " + operandTwo : ""
+      `Parsing the instruction ${operation} with operand one is ${operandOne} ${
+        operandTwo ? " and operand two is " + operandTwo : "."
       }`
     );
 
@@ -224,15 +395,18 @@ const Canvas = () => {
         const src = operandTwo;
         const dest = operandOne;
         mov(src, dest);
-
         break;
 
       case "push":
         push(operandOne);
+        break;
 
+      case "pop":
+        pop(operandOne);
         break;
 
       default:
+        consoleError("Invalid operation given.");
         break;
     }
   };
@@ -462,7 +636,14 @@ const Canvas = () => {
         </div>
       </div>
 
-      {loadingState && <Loader handleClose={() => setLoadingState(false)} />}
+      {loadingState && (
+        <Loader
+          handleClose={() => {
+            consoleLog("Initialization completed.");
+            setLoadingState(false);
+          }}
+        />
+      )}
     </div>
   );
 };
