429a670b1f60bb38531e284a58920d896e1bfe91
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-04-04 15:23:18 +0530

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  enabled: true,
-  mode: "jit",
-  content: [
-    // Example content paths...
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx,vue}",
-  ],
+    enabled: true,
+    mode: "jit",
+    content: [
+        // Example content paths...
+        "./public/**/*.html",
+        "./src/**/*.{js,jsx,ts,tsx,vue}",
+    ],
 
-  darkMode: "class", // class, 'media' or boolean
-  theme: {
-    extend: {
-      colors: {
-        gray: {
-          900: "#202225",
-          800: "#2f3136",
-          700: "#36393f",
-          600: "#4f545c",
-          400: "#d4d7dc",
-          300: "#e3e5e8",
-          200: "#ebedef",
-          100: "#f2f3f5",
+    darkMode: "class", // class, 'media' or boolean
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    900: "#202225",
+                    800: "#2f3136",
+                    700: "#36393f",
+                    600: "#4f545c",
+                    400: "#d4d7dc",
+                    300: "#e3e5e8",
+                    200: "#ebedef",
+                    100: "#f2f3f5",
+                },
+                primary: "#143b46",
+                secondary: "#2b6d7e",
+            },
+            spacing: {
+                88: "22rem",
+            },
+
+            gridTemplateColumns: {
+                24: "repeat(24, minmax(0, 1fr))",
+            },
         },
-        primary: "#143b46",
-        secondary: "#2b6d7e",
-      },
-      spacing: {
-        88: "22rem",
-      },
-
-      gridTemplateColumns: {
-        24: "repeat(24, minmax(0, 1fr))",
-      },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {},
+    plugins: [],
 };
