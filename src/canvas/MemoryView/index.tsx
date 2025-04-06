a60f2a3ac4500c563a2f58b2c16bd3e903b3b70e
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-04-06 23:18:51 +0530

    memory view update

--- src/canvas/MemoryView/index.tsx
+++ src/canvas/MemoryView/index.tsx
@@ -3,188 +3,191 @@ import { useForm } from "@hooks";
 import { useEffect, useState } from "react";
 
 interface Cell {
-  address: number;
-  value: number;
+    address: number;
+    value: number;
 }
 
-const MemoryView = ({ startAddress, wordCount, memory }) => {
-  const { formData, handleChange } = useForm({
-    startAddress: startAddress || 2000,
-    wordCount: wordCount || 24,
-    wordSize: "1",
-    displayType: "hexadecimal",
-  });
-
-  const [memoryCells, setMemoryCells] = useState<Cell[]>([]);
-  const [status, setStatus] = useState("...");
-  const [selection, setSelection] = useState<{
-    start: number | null;
-    end: number | null;
-  }>({
-    start: null,
-    end: null,
-  });
-
-  const formatValue = (value: number, displayType: string): string => {
-    switch (displayType) {
-      case "hexadecimal":
-        return value.toString(16).padStart(2, "0").toUpperCase();
-      case "binary":
-        return value.toString(2).padStart(8, "0");
-      case "octal":
-        return value.toString(8);
-      case "decimal":
-        return value.toString(10);
-      default:
-        return value.toString(16).padStart(2, "0").toUpperCase();
-    }
-  };
-
-  const buildMemoryView = () => {
-    let cells = [];
-
-    for (let i = 0; i < parseInt(formData.wordCount); i++) {
-      const address =
-        parseInt(formData.startAddress) + i * parseInt(formData.wordSize);
-
-      // Fallback to 0 if undefined
-      const value = memory[address] ? parseInt(memory[address], 2) : 0;
-
-      console.log(parseInt(memory[address]));
-
-      cells.push({
-        address,
-        value,
-      });
-    }
-
-    setMemoryCells(cells);
-  };
-
-  useEffect(() => {
-    buildMemoryView();
-  }, [formData]);
-
-  const handleHover = ({ address, value }) => {
-    setStatus(`Memory cell at ${address} with the value ${value}`);
-  };
-
-  const handleCellClick = (address: number) => {
-    setSelection((prev) => {
-      if (prev.start === null) {
-        return { start: address, end: null }; // Set start address
-      } else if (prev.end === null) {
-        return { start: prev.start, end: address }; // Set end address
-      } else {
-        return { start: address, end: null }; // Reset selection
-      }
+const MemoryView = ({ startAddress, wordCount = 24 }) => {
+    const { formData, handleChange } = useForm({
+        startAddress: startAddress || 2000,
+        wordCount: wordCount,
+        wordSize: "1",
+        displayType: "hexadecimal",
     });
-  };
 
-  const isSelected = (address: number) => {
-    const { start, end } = selection;
-    return start !== null && end !== null && address >= start && address <= end;
-  };
+    const [memoryCells, setMemoryCells] = useState<Cell[]>([]);
+    const [status, setStatus] = useState("...");
+    const [selection, setSelection] = useState<{
+        start: number | null;
+        end: number | null;
+    }>({
+        start: null,
+        end: null,
+    });
 
-  return (
-    <div className="w-full">
-      <div className="grid grid-cols-4 gap-2">
-        <Input
-          label="Start address"
-          name={"startAddress"}
-          value={formData.startAddress}
-          handleChange={handleChange}
-        />
-        <Input
-          label="Word count"
-          name={"wordCount"}
-          type="number"
-          value={formData.wordCount}
-          handleChange={handleChange}
-        />
-        <Select
-          label="Word size"
-          optList={[
-            {
-              id: "1",
-              title: "1 Bytes",
-            },
-            {
-              id: "2",
-              title: "2 Bytes",
-            },
-            {
-              id: "4",
-              title: "4 Bytes",
-            },
-            {
-              id: "8",
-              title: "8 Bytes",
-            },
-          ]}
-          name={"wordSize"}
-          value={formData.wordSize}
-          handleChange={handleChange}
-        />
-        <Select
-          label="Display in"
-          optList={[
-            {
-              id: "hexadecimal",
-              title: "Hexadecimal",
-            },
-            {
-              id: "binary",
-              title: "Binary",
-            },
-            {
-              id: "octal",
-              title: "Octal",
-            },
-            {
-              id: "decimal",
-              title: "Decimal",
-            },
-          ]}
-          name={"displayType"}
-          value={formData.displayType}
-          handleChange={handleChange}
-        />
-      </div>
+    const formatValue = (value: number, displayType: string): string => {
+        switch (displayType) {
+            case "hexadecimal":
+                return value.toString(16).padStart(2, "0").toUpperCase();
+            case "binary":
+                return value.toString(2).padStart(8, "0");
+            case "octal":
+                return value.toString(8);
+            case "decimal":
+                return value.toString(10);
+            default:
+                return value.toString(16).padStart(2, "0").toUpperCase();
+        }
+    };
 
-      <div className="w-full py-2">{status}</div>
+    const buildMemoryView = () => {
+        let cells = [];
 
-      <div className="w-full p-2 grid grid-cols-24 gap-1 mt-4 mb-6">
-        {memoryCells.map((memoryCell) => (
-          <div
-            key={memoryCell.address}
-            className={`cursor-pointer text-sm p-2 text-center border border-gray-500 rounded-sm 
+        for (let i = 0; i < parseInt(formData.wordCount); i++) {
+            const address =
+                parseInt(formData.startAddress) +
+                i * parseInt(formData.wordSize);
+
+            // Fallback to 0 if undefined
+            const value = memory[address] ? parseInt(memory[address], 2) : 0;
+
+            console.log(parseInt(memory[address]));
+
+            cells.push({
+                address,
+                value,
+            });
+        }
+
+        setMemoryCells(cells);
+    };
+
+    useEffect(() => {
+        buildMemoryView();
+    }, [formData]);
+
+    const handleHover = ({ address, value }) => {
+        setStatus(`Memory cell at ${address} with the value ${value}`);
+    };
+
+    const handleCellClick = (address: number) => {
+        setSelection((prev) => {
+            if (prev.start === null) {
+                return { start: address, end: null }; // Set start address
+            } else if (prev.end === null) {
+                return { start: prev.start, end: address }; // Set end address
+            } else {
+                return { start: address, end: null }; // Reset selection
+            }
+        });
+    };
+
+    const isSelected = (address: number) => {
+        const { start, end } = selection;
+        return (
+            start !== null && end !== null && address >= start && address <= end
+        );
+    };
+
+    return (
+        <div className="w-full">
+            <div className="grid grid-cols-4 gap-2">
+                <Input
+                    label="Start address"
+                    name={"startAddress"}
+                    value={formData.startAddress}
+                    handleChange={handleChange}
+                />
+                <Input
+                    label="Word count"
+                    name={"wordCount"}
+                    type="number"
+                    value={formData.wordCount}
+                    handleChange={handleChange}
+                />
+                <Select
+                    label="Word size"
+                    optList={[
+                        {
+                            id: "1",
+                            title: "1 Bytes",
+                        },
+                        {
+                            id: "2",
+                            title: "2 Bytes",
+                        },
+                        {
+                            id: "4",
+                            title: "4 Bytes",
+                        },
+                        {
+                            id: "8",
+                            title: "8 Bytes",
+                        },
+                    ]}
+                    name={"wordSize"}
+                    value={formData.wordSize}
+                    handleChange={handleChange}
+                />
+                <Select
+                    label="Display in"
+                    optList={[
+                        {
+                            id: "hexadecimal",
+                            title: "Hexadecimal",
+                        },
+                        {
+                            id: "binary",
+                            title: "Binary",
+                        },
+                        {
+                            id: "octal",
+                            title: "Octal",
+                        },
+                        {
+                            id: "decimal",
+                            title: "Decimal",
+                        },
+                    ]}
+                    name={"displayType"}
+                    value={formData.displayType}
+                    handleChange={handleChange}
+                />
+            </div>
+
+            <div className="w-full py-2">{status}</div>
+
+            <div className="w-full p-2 grid grid-cols-24 gap-1 mt-4 mb-6">
+                {memoryCells.map((memoryCell) => (
+                    <div
+                        key={memoryCell.address}
+                        className={`cursor-pointer text-sm p-2 text-center border border-gray-500 rounded-sm 
       ${
-        isSelected(memoryCell.address)
-          ? "bg-blue-400 text-white"
-          : "bg-primary text-secondary"
+          isSelected(memoryCell.address)
+              ? "bg-blue-400 text-white"
+              : "bg-primary text-secondary"
       }
       ${
-        formData.wordSize == 8
-          ? "col-span-8"
-          : formData.wordSize == 4
-          ? "col-span-4"
-          : formData.wordSize == 2
-          ? "col-span-2"
-          : "col-span-1"
+          formData.wordSize == 8
+              ? "col-span-8"
+              : formData.wordSize == 4
+              ? "col-span-4"
+              : formData.wordSize == 2
+              ? "col-span-2"
+              : "col-span-1"
       }
       hover:bg-secondary hover:text-white`}
-            onMouseEnter={() => {
-              handleHover(memoryCell);
-            }}
-          >
-            {formData.displayType === "hexadecimal" ? "0x" : ""}
-            {formatValue(memoryCell.value, formData.displayType)}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+                        onMouseEnter={() => {
+                            handleHover(memoryCell);
+                        }}
+                    >
+                        {formData.displayType === "hexadecimal" ? "0x" : ""}
+                        {formatValue(memoryCell.value, formData.displayType)}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default MemoryView;
