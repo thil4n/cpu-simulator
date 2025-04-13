05c2b5009da74edf07b164190f9dd6d3b8284217
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-04-13 17:01:45 +0530

    memory plane

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -17,6 +17,7 @@ import MemoryBar from "./MemoryBar";
 import InstructionView from "./InstructionView";
 import ExecutionController from "./ExecutionController";
 import ProgramLoader from "./programLoader";
+import MemoryPlane from "./MemoryPlane";
 
 interface ExamineMemory {
     startAddress: number;
@@ -166,73 +167,8 @@ const App = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="bg-[#555] bg-opacity-50 backdrop-blur-lg">
-                        <h1 className="bg-primary text-secondary w-full py-1 text-sm text-center uppercase">
-                            Examine Memory
-                        </h1>
-                        <div className="px-2 py2 mt-3">
-                            <div className="grid grid-cols-3 gap-x-1">
-                                <Button
-                                    text={"Stack"}
-                                    handleClick={() => {
-                                        const rsp = bitArrayToNumber(
-                                            registers.rsp
-                                        );
 
-                                        logger.info("Showing the stack area.");
-
-                                        showMemory(rsp);
-                                    }}
-                                />
-                                <Button
-                                    text={"Heap"}
-                                    handleClick={function (): void {
-                                        throw new Error(
-                                            "Function not implemented."
-                                        );
-                                    }}
-                                />
-                                <Button
-                                    text={".BSS"}
-                                    handleClick={function (): void {
-                                        throw new Error(
-                                            "Function not implemented."
-                                        );
-                                    }}
-                                />
-                                <Button
-                                    text={".TXT"}
-                                    handleClick={() => {
-                                        const rip = bitArrayToNumber(
-                                            registers.rip
-                                        );
-
-                                        logger.info(
-                                            "Showing the text segment."
-                                        );
-
-                                        showMemory(rip);
-                                    }}
-                                />
-                                <Button
-                                    text={".DATA"}
-                                    handleClick={function (): void {
-                                        throw new Error(
-                                            "Function not implemented."
-                                        );
-                                    }}
-                                />
-                                <Button
-                                    text={"Address"}
-                                    handleClick={function (): void {
-                                        throw new Error(
-                                            "Function not implemented."
-                                        );
-                                    }}
-                                />
-                            </div>
-                        </div>
-                    </div>
+                    <MemoryPlane showMemory={showMemory} />
                 </div>
             </div>
 
