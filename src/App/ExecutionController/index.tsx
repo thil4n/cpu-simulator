fac2228a88bfdbcf5b7f47e82719caafd3b7409e
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-04-13 09:01:56 +0530

    fix execution

--- src/App/ExecutionController/index.tsx
+++ src/App/ExecutionController/index.tsx
@@ -6,7 +6,7 @@ import {
 import { bitArrayToNumber, parseSingleLine } from "@utils";
 import { disassemble } from "@lib";
 
-import { Button, Input } from "@components";
+import { Button } from "@components";
 
 import useInstructions from "../../instructions/useInstructions";
 
@@ -15,15 +15,10 @@ const ExecutionController = () => {
     const { registers } = useRegisterContext();
     const logger = useLoggerContext();
 
-    let rip = bitArrayToNumber(registers.rip);
-
     const { push, mov } = useInstructions();
 
-    const parseAssembly = () => {
-        const instruction = "formData.assemblyInput";
-
-        const { operation, operandOne, operandTwo } =
-            parseSingleLine(instruction);
+    const execute = (line: string) => {
+        const { operation, operandOne, operandTwo } = parseSingleLine(line);
 
         logger.info(
             `Parsing the instruction ${operation} with operand one is ${operandOne} ${
@@ -48,6 +43,15 @@ const ExecutionController = () => {
         }
     };
 
+    const executeCurrentInstruction = () => {
+        let rip = bitArrayToNumber(registers.rip);
+        const opcode = getMemoryBytes(rip, 10);
+
+        const instructionLine = disassemble(opcode);
+
+        execute(instructionLine.instruction);
+    };
+
     return (
         <div className="bg-[#555] bg-opacity-50 backdrop-blur-lg mt-2">
             <h1 className="bg-primary text-secondary w-full py-1 text-sm text-center uppercase mb-3">
@@ -55,8 +59,11 @@ const ExecutionController = () => {
             </h1>
             <div className="px-2">
                 <div className="grid grid-cols-2 gap-1 -mt-1">
-                    <Button text="Execute" handleClick={parseAssembly} />
-                    <Button text="Custom" handleClick={parseAssembly} />
+                    <Button
+                        text="Execute"
+                        handleClick={() => executeCurrentInstruction()}
+                    />
+                    <Button text="Custom" handleClick={null} />
                 </div>
             </div>
         </div>
