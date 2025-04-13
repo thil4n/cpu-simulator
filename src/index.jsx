5f6a818a9a1e42b2702b9c46c88465b522153fe2
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-04-13 09:09:54 +0530

    remove obsolete index.tsx

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/css/main.css";
-import "react-toastify/dist/ReactToastify.css";
-
-import Canvas from "./canvas";
-
-ReactDOM.render(<Canvas />, document.getElementById("root"));
